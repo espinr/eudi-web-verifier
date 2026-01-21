e354b6c245dddcd7d9c6df38cf75f63467000314
espinr <martin.alvarez.espinar1@huawei.com>
2026-01-21 14:56:07 +0100

    Check Dates as attributes

--- src/app/features/invoke-wallet/components/view-attestation/view-attestation.component.ts
+++ src/app/features/invoke-wallet/components/view-attestation/view-attestation.component.ts
@@ -64,12 +64,15 @@ export class ViewAttestationComponent implements OnInit {
   }
 
   isObject(value: any): boolean {
-    console.log(JSON.stringify(value));
     return value !== null && typeof value === 'object' && !Array.isArray(value);
   }
 
+  // Date is in {'value':'YYYY-MM-DD'} format
+  isDate(key: string, value:any): boolean {
+    return value !== null && typeof value === 'object' && key.includes('expiry_date') && value.containsKey('value');
+  }
+
   isArray(value: any): boolean {
-    console.log(JSON.stringify(value));
     return Array.isArray(value);
   }
 
